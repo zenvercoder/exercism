215d81bbdba7e92e515a510187354e46a673810d
Lisa Hull <lisa@jchull.com>
2016-08-23 09:44:23 -0600

    hello-world initial commit

---
+++ hello-world.js
@@ -0,0 +1,12 @@
+function HelloWorld(name){
+    if(name == null){
+        console.log('Hello, World!');
+    }
+    else {
+        console.log('Hello, ' + name);
+    }
+
+}
+
+HelloWorld();
+HelloWorld('jeremy');
